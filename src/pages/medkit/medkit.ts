d6edb8ecd0f80f5ed829906cee1f72942b9b38b7
shangkaul <shangkaul@gmail.com>
2017-08-12 05:20:42 +0530

    hosp backend

--- src/pages/medkit/medkit.ts
+++ src/pages/medkit/medkit.ts
@@ -3,7 +3,8 @@ import {NavController, NavParams } from 'ionic-angular';
   import { Geolocation } from '@ionic-native/geolocation';
 import { ContactProvider } from '../../providers/contact/contact';
 import { SMS } from '@ionic-native/sms';
-
+import {Http} from '@angular/http';
+import {UserProvider}  from '../../providers/user/user';
 /**
  * Generated class for the MedkitPage page.
  *
@@ -19,7 +20,7 @@ export class MedkitPage {
 public lat:any;
 public long:any;
   constructor(public navCtrl: NavController, public navParams: NavParams,private geolocation: Geolocation,private sms: SMS,
-  public con:ContactProvider) {
+  public con:ContactProvider,public http:Http,public user:UserProvider) {
   
 this.geolocation.getCurrentPosition().then((resp) => {
   console.log(resp.coords.latitude+","+resp.coords.longitude);
@@ -42,10 +43,19 @@ watch.subscribe((data) => {
   }
 help()
 {
+  this.http.get("http://srmpedia.esy.es/health/save.php?lat="+this.lat+"&long="+this.long+"&nam="+this.user.name+"&mob="+this.user.uid)
+ .subscribe((res)=>{
+ 
 
+  });
+ this.sms.send(this.con.n1, 'Mar rha hu  https://www.google.co.in/maps/dir//'+this.lat+','+this.long);
+ this.sms.send(this.con.n2, 'Mar rha hu  https://www.google.co.in/maps/dir//'+this.lat+','+this.long);
  this.sms.send(this.con.n3, 'Mar rha hu  https://www.google.co.in/maps/dir//'+this.lat+','+this.long);
+ this.sms.send(this.con.n4, 'Mar rha hu  https://www.google.co.in/maps/dir//'+this.lat+','+this.long);
 this.sms.send(this.con.n5, 'Mar rha hu  https://www.google.co.in/maps/dir//'+this.lat+','+this.long);
 alert("SENT");
+
+
 
 }
 
