28b78d73ed3545d452a77bb91b8b8ba7b029fb13
yash050499 <yaggarwal58@gmail.com>
2017-08-11 20:39:40 +0530

    ui changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import {GoogleLoginPage} from '../pages/google-login/google-login';
+import {SignUpPage} from '../pages/sign-up/sign-up';
 import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any = SignUpPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
